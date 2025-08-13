fbb9f02b8bc6fdd4fc2cccc44460960f621fd64b
Pradeep <pradeep.ge24@gmail.com>
2025-08-14 00:45:22 +0530

    Base App Created 🚀

---
+++ templates/template-react-mui-all/src/AppContainer.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./config/theme.js";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ templates/template-react-mui-basic/src/App.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <h1>Hello,World!</h1>
+    </Box>
+  );
+}
+
+export default App;
